3fbd13a15769d564b160cd82ecc6b42cb9a2eeec
sunmin <polarlsm1231@gmail.com>
2023-03-28 14:35:34 +0900

    Greedy: 큰수만들기

---
+++ 프로그래머스/Greedy/큰수만들기.py
@@ -0,0 +1,14 @@
+def solution(number, k):
+    n = len(number)
+    ans = []
+    for i in range(n):
+        for j in range(len(ans)-1, -1, -1):
+            if ans[j] < number[i] and k > 0:
+                ans.pop()
+                k -= 1
+            else:
+                break
+        ans.append(number[i])
+    if k > 0:
+        return "".join(ans[:-1*k])
+    return "".join(ans)
